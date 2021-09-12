fed5e627f18f324c9fed016fd1615cdf510eedbc
Charlie <charlie_jb@hotmail.co.uk>
2021-09-12 17:33:21 +0100

    Added hamburger menu for smaller screens

--- collapse.js
+++ collapse.js
@@ -1,1 +1,23 @@
-console.log("loaded")
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const divider = document.querySelector("#divider");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    console.log("toggle")
+    hamburger.classList.toggle("activeHam");
+    navMenu.classList.toggle("activeHam");
+    divider.classList.toggle("activeHam");
+
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("activeHam");
+    navMenu.classList.remove("activeHam");
+    divider.classList.remove("activeHam");
+}
